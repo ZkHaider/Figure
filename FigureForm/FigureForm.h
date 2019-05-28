151d18fdde6cd79185cf89ed5e25fa47a7f66207
Haider Khan <hkhan@datalofts.com>
2019-05-28 02:35:14 -0700

    Added form dependency for target FigureForm

---
+++ FigureForm/FigureForm.h
@@ -0,0 +1,19 @@
+//
+//  FigureForm.h
+//  FigureForm
+//
+//  Created by Haider Khan on 5/28/19.
+//  Copyright © 2019 Figure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FigureForm.
+FOUNDATION_EXPORT double FigureFormVersionNumber;
+
+//! Project version string for FigureForm.
+FOUNDATION_EXPORT const unsigned char FigureFormVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FigureForm/PublicHeader.h>
+
+
